90c249ffe0a4802222572be571e7cdddaef971a1
david-soto-m <davidsotomarchena@protonmail.com>
2020-08-22 15:09:17 +0200

    maoin.py added

---
+++ main.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+import sys
+import PyQt5.QtWidgets as QW
+import PyQt5.QtGui as QG
+import PyQt5.QtCore as QC
+
+#class centralWidget(QW.QWidget):
+	#def __init__(self):
+		#super().__init__()
+		#self.defineElements()
+		#self.defineLayout()
+	
+	#def defineElements(self):
+		#pass
+	#def defineLayout(self):
+		#pass
+
+class GuiApp(QW.QMainWindow):
+	def __init__(self):
+		super().__init__()
+		self.defineMenuBar()
+		#self.cwidg=centralWidget()
+		#self.setCentralWidget(self.cwidg)
+		self.show()
+		#a=auxsz();
+		#a.toscalescreen(self,-1,1)
+	def defineMenuBar(self):
+		exitAct = QW.QAction('&Quit', self)
+		exitAct.setShortcut('Ctrl+Q')
+		exitAct.triggered.connect(QW.qApp.quit)
+		fileMenu = self.menuBar().addMenu('&File')
+		fileMenu.addAction(exitAct)
+
+def main():
+	app=QW.QApplication(sys.argv)
+	ex=GuiApp()
+	sys.exit(app.exec_())
+if __name__=="__main__":
+	main()
