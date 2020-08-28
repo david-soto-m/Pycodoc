80990847e9916e68def1aa320aa5f12c2de96df3
david-soto-m <davidsotomarchena@protonmail.com>
2020-08-28 20:09:47 +0200

    firstXML

--- MainWindow/ToolBar/ToolBar.py
+++ MainWindow/ToolBar/ToolBar.py
@@ -1,6 +1,7 @@
 import PyQt5.QtWidgets as QW
 import PyQt5.QtGui as QG
 import PyQt5.QtCore as QC
+import xml.etree.ElementTree as ET
 
 class ToolBar():
 	def __init__(self,parent):
@@ -12,6 +13,7 @@ class ToolBar():
 		
 		self.toolBar.addAction(self.qacts.back)
 		self.toolBar.addAction(self.qacts.ahead)
+		self.toolBar.addWidget(self.combosearch.swid)
 
 class backnextact():
 	def __init__(self):
@@ -23,6 +25,29 @@ class backnextact():
 		self.ahead.setIcon(QG.QIcon().fromTheme("go-next"))
 		self.ahead.setToolTip("Forward")
 
+class fileElement():
+	def __init__(self,singleElement):
+		self.title=singleElement.find("title")
+		self.direc=singleElement.find("dir")
+		self.name=singleElement.find("name")
+
 class searchWidg():
 	def __init__(self):
-		pass
+		self.swid=QW.QComboBox()
+		self.swid.setEditable(True)
+		self.swid.setCurrentIndex(-1)
+		self.swid.activated.connect(self.comboChanged)
+		self.swid.setSizeAdjustPolicy(self.swid.SizeAdjustPolicy.AdjustToContents)
+		
+		tree = ET.parse('./config/Files.xml')
+		root = tree.getroot()
+		
+		for child in root.iter("Elem"):
+			Elem=fileElement(child);
+			self.swid.addItem(Elem.title.text,QC.QVariant([Elem.direc.text,Elem.name.text]))
+	
+	def comboChanged(self,idx):
+		if idx==0:
+			print("idx 0:",self.swid.itemData(idx,0x100))
+		else:
+			print("idx 1:",self.swid.itemData(idx,0x100))
--- main.py
+++ main.py
@@ -1,10 +1,23 @@
 #!/usr/bin/python3
-
+import os
 import sys
 from PyQt5.QtWidgets import QApplication
 from MainWindow.MainWindow import GuiApp
 
+def pathToRootOfProject():
+	direc=__file__
+	notthere=True
+	idx=len(direc)-1
+	while notthere:
+		direc=direc[0:idx]
+		idx-=1
+		if direc[idx]=='/':
+			notthere=False
+	return direc
+
 def main():
+	os.chdir(pathToRootOfProject())
+	
 	app=QApplication(sys.argv)
 	ex=GuiApp()
 	sys.exit(app.exec_())
