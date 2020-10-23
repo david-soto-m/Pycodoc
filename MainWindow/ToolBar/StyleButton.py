734200bc81559f746a344ca374d8d775e8ef8509
david-soto-m <davidsotomarchena@protonmail.com>
2020-10-23 17:56:50 +0200

    Now the readme files can be remotely useful

--- MainWindow/ToolBar/StyleButton.py
+++ MainWindow/ToolBar/StyleButton.py
@@ -2,6 +2,7 @@ from PyQt5.QtWidgets import QAction, QMenu
 from PyQt5.QtGui import QIcon
 from pathlib import Path
 from FileManage.fileElement import fileElement
+from AuxWindows.StyleWidget import StyleWidget
 import glob_objects.globalxml as GXML
 
 class styleButton(QAction):
@@ -13,6 +14,7 @@ class styleButton(QAction):
 		self.setMenu(self.styleMenu())
 		self.hovered.connect(self.refreshMenu)
 		self.triggered.connect(self.triggerOpen)
+		self.styleCreator=StyleWidget()
 	
 	def refreshMenu(self):
 		self.setMenu(self.styleMenu())
@@ -20,21 +22,20 @@ class styleButton(QAction):
 	def styleMenu(self):
 		self.actions=[]
 		Menu=QMenu()
-		for child,index in zip(GXML.styleLocsRoot.findall("Elem[@show='True']"),range(len(GXML.styleLocsRoot))):
+		for child in GXML.styleLocsRoot.findall("Elem[@show='True']"):
 			Elem=fileElement(child,style=True)
 			self.actions.append(QAction(Elem.title.text))
-			self.actions[index].setData(Elem)
-			self.actions[index].triggered.connect(self.trigger)
-			Menu.addAction(self.actions[index])
-		index=len(self.actions)
-		#self.actions.append(QAction('Base'))
-		#self.actions[index].setData(None)
-		#self.actions[index].triggered.connect(self.trigger)
-		#Menu.addAction(self.actions[index])
+			self.actions[-1].setData(Elem)
+			self.actions[-1].triggered.connect(self.trigger)
+			Menu.addAction(self.actions[-1])
+		
+		self.actions.append(QAction('Create Style'))
+		self.actions[-1].triggered.connect(self.styleCreator.show())
+		Menu.addAction(self.actions[-1])
+		
 		return Menu
 	
 	def triggerOpen(self,boolean):
-		print("here")
 		home_dir=str(Path.home())
 		fname=QFileDialog.getOpenFileNames(caption='Open file',directory=home_dir)
 		if fname[0]:
--- pycodoc.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-#A CLI for pycodoc, providing access and modification capacity to config files.
-import sys
-def main ():
-    pass
-if __name__=="main":
-    main()
