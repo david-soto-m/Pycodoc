fb7ab6666dc9ad2d915aba98b48b56daa702916a
david-soto-m <davidsotomarchena@protonmail.com>
2020-10-25 18:39:04 +0100

    add to menu

--- installer.py
+++ installer.py
@@ -1,7 +1,16 @@
 #!/usr/bin/python3
 # An installer that will create a dektop file and try to add it to your apps menu
-import sys
+from pathlib import Path
+from os import geteuid
 def main ():
-    pass
-if __name__=="main":
-    main()
+	path1=str(Path(__file__).parent.resolve())+"/main.py"
+	path2=str(Path(__file__).parent.resolve())+"/AppIcon/AppIcon.svg"
+	stri="[Desktop Entry]\nEncoding=UTF-8\nType=Application\nTerminal=false\nCategories=Development;Documentation\nExec=%s\nName=Pycodoc\nIcon=%s"% (path1,path2)
+	if geteuid() == 0:
+		target="/usr/share/applications/Pycodoc.desktop"
+	else:
+		target=str(Path.home())+"/.local/share/applications/Pycodoc.desktop"
+	with open(target,"w+") as f:
+		f.write(stri)
+if __name__=="__main__":
+	main()
